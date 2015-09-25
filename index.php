450b4b09ed5395f89d774db591b405644f193d9b
Luca Tumedei <luca@theaveragedev.com>
2015-09-25 10:28:03 +0200

    cleaned the theme to bare bones

--- index.php
+++ index.php
@@ -1,55 +1,81 @@
+<?php get_header(); ?>
+<style>
+	.centered {
+		  width: 95%;
+		  margin: 2rem auto;
+		  background: #f0c9b9;
+		color: darkslategray;
+		padding:1rem;
+	}
+	.lazy-img{
+		padding: 1rem;
+	}
+	.lazy-img > img{
+		width: 100%;
+	}
+	.empty-space {
+	  height: 100rem;
+	}
+	.empty-space > p{
+		position: relative;
+		top: 50%;
+		transform: translateY(-50%);
+		text-align: center;
+		font-size: 300%;
+	}
+</style>
 <?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Lazy
- */
-
-get_header(); ?>
+	$ic_active = function_exists( 'ic_load' ) ? 'active' : 'not active';
+	$wp_routes_active = function_exists('respond') ? 'active' : 'not active';
+	?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<header class="centered padded">
+				<h2>Here are some images</h2>
+				<p>Some will be immediately visible and some will not.</p>
+				<p>If <a href="http://intercoolerjs.org/">intercooler.js</a> and <a href="https://github.com/lucatume/wp-routes"></a> are active then lazy loading of images should happen.</p>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+				<p>intercooler.js is <b><?php echo $ic_active ?></b></p>
+				<p>wp-routes is <b><?php echo $wp_routes_active ?></b></p>
+			</header>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="empty-space centered">
+				<p>Big empty space, scroll down and see lazy loading happening.</p>
+			</div>
 
-				<?php
+			<div class="lazy-img centered" ic-trigger-on="scrolled-into-view"
+			      ic-src="/lazy-load-image?url=<?php echo urlencode( 'http://lorempixel.com/800/400/sports/1' ) ?>">
+			</div>
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+			<div class="empty-space centered">
+				<p>Another empty space, keep scrolling.</p>
+			</div>
 
-			<?php endwhile; ?>
+			<div class="lazy-img centered" ic-trigger-on="scrolled-into-view"
+			      ic-src="/lazy-load-image?url=<?php echo urlencode( 'http://lorempixel.com/800/400/sports/2' ) ?>">
+			</div>
 
-			<?php the_posts_navigation(); ?>
+			<div class="empty-space centered">
+				<p>Yeah. More empty space, go on.</p>
+			</div>
 
-		<?php else : ?>
+			<div class="lazy-img centered" ic-trigger-on="scrolled-into-view"
+			      ic-src="/lazy-load-image?url=<?php echo urlencode( 'http://lorempixel.com/800/400/sports/3' ) ?>">
+			</div>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<div class="empty-space centered">
+				<p>Last one. Last scrolling effort.</p>
+			</div>
 
-		<?php endif; ?>
+			<div class="lazy-img centered" ic-trigger-on="scrolled-into-view"
+			      ic-src="/lazy-load-image?url=<?php echo urlencode( 'http://lorempixel.com/800/400/sports/4' ) ?>">
+			</div>
 
-		</main><!-- #main -->
+		</main>
+		<!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_footer();
